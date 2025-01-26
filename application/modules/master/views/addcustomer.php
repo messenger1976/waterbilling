b5b0d487832ff252c53a592e89de80689d666f9a
Herald Felisilda <herald_felisilda@yahoo.com>
2025-01-26 22:39:58 +0800

    Update customer info by addign membership status

--- application/modules/master/views/addcustomer.php
+++ application/modules/master/views/addcustomer.php
@@ -1,24 +1,4 @@
 <!DOCTYPE html>
-<html lang="en-us">
-	<head>
-		<meta charset="utf-8">
-		<!--<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">-->
-
-		<title> SmartAdmin </title>
-		<meta name="description" content="">
-		<meta name="author" content="">
-			
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-
-		<!-- FAVICONS -->
-		<link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
-		<link rel="icon" href="img/favicon/favicon.ico" type="image/x-icon">
-
-		<!-- GOOGLE FONT -->
-		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,300,400,700">
-
-	</head>
-	
 <!-- MAIN PANEL -->
 		<div id="main" role="main">
 
@@ -236,13 +216,13 @@
 										</div>
 										<!-- end widget content -->
 				                    
-									 <div>&nbsp;</div>
+									 <!--<div>&nbsp;</div>
 									  <div class="row">
 									   <div class="col-lg-12">
                                         	<input type="submit" class="btn btn-sm btn-primary" name="add" id="add" value="Delete All" onClick="return deleteAllData();" />
                                          </div>
 									</div>
-									 <div>&nbsp;</div>
+									 <div>&nbsp;</div>-->
 									  </form> 
 								</div>
 								<!-- end widget div -->
