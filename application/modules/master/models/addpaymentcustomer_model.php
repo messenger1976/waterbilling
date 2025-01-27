1e6a71894a8358a33dc9893f1af74136f7abcfcf
Herald Felisilda <herald_felisilda@yahoo.com>
2025-01-27 08:36:56 +0800

    Update customer invoice view

--- application/modules/master/models/addpaymentcustomer_model.php
+++ application/modules/master/models/addpaymentcustomer_model.php
@@ -15,6 +15,7 @@ class addpaymentcustomer_model extends CI_Model {
 	public $table_doc_series_number ='tbl_doc_series_number';
 	public $table_zone ='tbl_zone';
 	public $table_meter_reading ='tbl_addcustomer_reading';
+	public $table_users ='tbl_responsibilities_user';
 	// Autoloading a system library usin constructor method
 	public function __construct() {
         parent::__construct();
@@ -280,12 +281,14 @@ class addpaymentcustomer_model extends CI_Model {
     public function get_single_record($id='') {
         $this->db->select($this->table_name.".*,".$this->table_zone.".zone as zonename, ".$this->table_meter_reading.".unit_price,".$this->table_meter_reading.".penalty,
 		".$this->table_meter_reading.".amount,
-		".$this->table_meter_reading.".sc_discount
+		".$this->table_meter_reading.".sc_discount,
+		".$this->table_users.".employee_name
 		");
 		$this->db->from($this->table_name);
 		$this->db->join($this->table_meter_reading,$this->table_name.'.customer_id='.$this->table_meter_reading.'.customer_id AND '.$this->table_name.'.month='.$this->table_meter_reading.'.month AND '.$this->table_name.'.year='.$this->table_meter_reading.'.year','left');
 		$this->db->join($this->table_customers,$this->table_name.'.customer_id='.$this->table_customers.'.customer_id','left');
 		$this->db->join($this->table_zone,$this->table_customers.'.zone='.$this->table_zone.'.id','left');
+		$this->db->join($this->table_users,$this->table_name.'.userid='.$this->table_users.'.id','left');
 		if($id != ''){
 			$this->db->where($this->table_name.".id",$id);
 			$query = $this->db->get();
--- application/modules/master/views/month_customer_invoice.php
+++ application/modules/master/views/month_customer_invoice.php
@@ -90,7 +90,8 @@
 																echo "Un-Paid";
 															}
 															?></b><br/><br/>
-										<span class="name">Paid Date : 	<?php echo date("M d, Y ", strtotime($record['create_date_time'])); ?></span><br/>
+										<span class="name">Paid Date : 	<?php echo date("M d, Y ", strtotime($record['date'])); ?></span>
+										<span class="name">Cashier/Teller : 	<?php echo strtoupper($record['employee_name']); ?></span><br/>
 										<span class="name">Admin Address : 	<?php echo (stripslashes(str_replace('\n','',$address['content']))); ?></span>
 									</div>
 									
