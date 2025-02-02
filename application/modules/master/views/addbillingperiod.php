9514dc9844dbb190b4738c28faad8a497385a4b3
Herald Felisilda <herald_felisilda@yahoo.com>
2025-02-03 02:19:35 +0800

    Update import excel

--- application/modules/master/views/addbillingperiod.php
+++ application/modules/master/views/addbillingperiod.php
@@ -104,6 +104,8 @@
                                                 <a class="btn btn-sm btn-success" name="balanceforward" id="balanceforward" value="Close" data-toggle="modal" data-target="#myModal">Balance Forward</a>
 												<a id="exporttoexcel" href="<?php echo ADMIN_URL;?>addbillingperiod/fileDownloadunpaidSearch/<?php if($this->input->post('customer_type')!=''){ echo $this->input->post('customer_type'); }else{ echo 0;} ?>/<?php if($this->input->post('zone')!=''){ echo $this->input->post('zone'); }else{ echo 0;} ?>/<?php if($this->input->post('fromdate')!=''){ echo $this->input->post('fromdate'); }else{ echo 0;} ?>/<?php if($this->input->post('todate')!=''){ echo $this->input->post('todate'); }else{ echo 0;} ?>
 																		" class="btn btn-sm btn-primary" style="margin-bottom: 4px;">Export Excel for Mobile</a>
+
+																		<a href="<?php echo ADMIN_URL.'addbillingperiod/import'; ?>"class="btn btn-sm btn-info" style="margin-bottom: 5px;">Import Excel from Mobile</a>
                                             </div>
                                             </legend>
                                                 
