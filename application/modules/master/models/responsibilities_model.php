74ad96bada87802bbb7935631260ddc51435f6a2
Herald Felisilda <herald_felisilda@yahoo.com>
2025-02-19 00:53:53 +0800

    Update fix bug

--- application/modules/master/models/responsibilities_model.php
+++ application/modules/master/models/responsibilities_model.php
@@ -234,7 +234,7 @@ class responsibilities_model extends CI_Model {
 	}	
 	
 	public function get_income_metercustomer(){
-		$this->db->select('SUM(grand_amount) as total1');
+		$this->db->select('SUM(grand_total) as total1');
 		$this->db->from($this->table_meter);
 		$query = $this->db->get();
 		$result = $query->row_array();
