38b094a91d9c34c6b31fce126320fafe3d9e8240
Herald Felisilda <herald_felisilda@yahoo.com>
2025-03-23 22:32:02 +0800

    Update leaking menu module

--- application/views/admin-includes/navigation.php
+++ application/views/admin-includes/navigation.php
@@ -56,7 +56,7 @@
 								<?php if($this->uri->segment(2)=='paymentmonthlycustomer'){echo 'active open';}?>
 								<?php if($this->uri->segment(2)=='addpaymentcustomer'){echo 'active open';}?>
 								<?php if($this->uri->segment(2)=='amountrate'){echo 'active open';}?>	
-								<?php if($this->uri->segment(2)=='leakingentry'){echo 'active open';}?>							   
+								<?php if($this->uri->segment(2)=='Leakingentry'){echo 'active open';}?>							   
 						     ">
 						<a href="#"><i class="fa fa-lg fa-fw fa-money"></i> <span class="menu-item-parent">Finance</span></a>
 						<ul>
@@ -69,8 +69,8 @@
 							</li>
 							
 							<?php if((array_key_exists('leakingentry',$roleResponsible) && ($roleResponsible['leakingentry'] == 1) ) || ($this->session->userdata('usertype') == 'admin')){ ?>	
-							<li class="<?php if($this->uri->segment(2)=='leakingentry'){echo 'active';}?>">
-								<a href="<?php echo ADMIN_URL;?>leakingentry">Leaking Entry</a>
+							<li class="<?php if($this->uri->segment(2)=='Leakingentry'){echo 'active';}?>">
+								<a href="<?php echo ADMIN_URL;?>Leakingentry">Leaking Entry</a>
 							</li>
 							<?php } ?>
 
