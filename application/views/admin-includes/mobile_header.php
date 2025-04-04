08acef1549cc92b6fa494145fa040e94fb6d9779
Herald Felisilda <herald_felisilda@yahoo.com>
2025-04-04 18:04:58 +0800

    Update mobile app

--- application/views/admin-includes/mobile_header.php
+++ application/views/admin-includes/mobile_header.php
@@ -78,7 +78,39 @@ if(($this->session->userdata('logged_in')!='ECOM')||($this->session->userdata('u
 			</div>
 
 			
+<!-- pulled right: nav area -->
+<div class="pull-right">
+				
+				<!-- collapse menu button -->
+				<div id="hide-menu" class="btn-header pull-right">
+					<span> <a href="javascript:void(0);" data-action="toggleMenu" title="Collapse Menu"><i class="fa fa-reorder"></i></a> </span>
+				</div>
+				<!-- end collapse menu -->
+				
+				<!-- #MOBILE -->
+				
 
+				<!-- logout button -->
+				<div id="logout" class="btn-header transparent pull-right">
+					<span> <a href="login.html" title="Sign Out" data-action="userLogout" data-logout-msg="You can improve your security further after logging out by closing this opened browser"><i class="fa fa-sign-out"></i></a> </span>
+				</div>
+				<!-- end logout button -->
+
+				
+				
+
+				<!-- fullscreen button -->
+				<div id="fullscreen1" class="btn-header transparent pull-right">
+					<span> <a href="javascript:void(0);" data-action="launchFullscreen" title="Full Screen"><i class="fa fa-arrows-alt"></i></a> </span>
+				</div>
+				<!-- end fullscreen button -->
+				
+				
+
+				
+
+			</div>
+			<!-- end pulled right: nav area -->
 			
 
 		</header>
