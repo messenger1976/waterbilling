d5304520459a625258313247e14e3152d5b4b93b
Herald Felisilda <herald_felisilda@yahoo.com>
2025-06-19 23:48:46 +0800

    Update monthly report

--- application/modules/master/views/monthly_billing_report_ajax.php
+++ application/modules/master/views/monthly_billing_report_ajax.php
@@ -129,7 +129,7 @@
 									  	echo "Paid"; 
 									  }elseif($row['customer_status']== '2'){
 										echo "Disconnected"; 
-									  }elseif($row['invoice_id']== '' && $row['consumed']=='0'){
+									  }elseif($row['customer_status']== '1' && ($row['reading']=='0' || is_null($row['reading']))){
 										echo "No Reading"; 
 									  }else{ 
 									  	echo "Un-Paid"; 
