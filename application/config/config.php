b2412910b87a78b4a0046ad9d4a8889ce9af128e
Herald Felisilda <herald_felisilda@yahoo.com>
2025-07-19 11:53:34 +0800

    Update git config

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,8 @@
 | path to your installation.
 |
 */
-$config['base_url'] = 'http://localhost/waterbilling/';
+//$config['base_url'] = 'http://localhost/waterbilling/';
+$config['base_url'] = 'http://roxas.com/';
 
 /*
 |--------------------------------------------------------------------------
