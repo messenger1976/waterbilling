69c0f7ecd6952618feee9107d5f4658d20643e24
Herald Felisilda <herald_felisilda@yahoo.com>
2026-08-11 22:51:01 +0800

    update config file

--- application/config/config.php
+++ application/config/config.php
@@ -276,7 +276,7 @@ $config['encryption_key'] = 'test';
 |
 */
 $config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
+$config['sess_expiration']		= 0;
 //$config['sess_expire_on_close']	= FALSE;  //default set this one
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
